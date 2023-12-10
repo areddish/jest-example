15099bb047ab1010de8bf710a1aeffc8e407539d
Andrew <30499261+areddish@users.noreply.github.com>
2023-12-09 22:30:54 -0500

    Update channel.js

--- channel.js
+++ channel.js
@@ -1,22 +1,8 @@
 import { Collection } from "./discord";
 
-export const someQueue = {
-    first: () => null,
-    push: (v) => console.log(`pushing ${v}`),
-    length: 123
-}
-
-export const addTwo = () => {
-    someQueue.push(1);
-    someQueue.push(2);
-    return someQueue.length;
-}
-
-export const myQueue = new Collection();
+const myQueue = new Collection();
 
 export const emptyQueue = () => {
-    console.log(JSON.stringify(myQueue));
-    console.log(JSON.stringify(myQueue.first));
     let obj = myQueue.first();
 
     // sort myQueue
